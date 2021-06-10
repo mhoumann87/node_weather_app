cba65da739ea4099589d71ecc4b4c5f1a68c2df5
Michael Houmann <mhoumann87@gmail.com>
2021-06-10 14:11:34 +0200

    Setup api call

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const request = require('postman-request');
+
 // Get the api key to weatherstack
 const apiKey = require('./api-keys');
 
-const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=New York`;
-
+const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=Copenhagen`;
 const encodedUrl = encodeURI(url);
 
-console.log(encodedUrl);
+request({ url: encodedUrl }, (err, res) => {
+  console.log(res);
+});
