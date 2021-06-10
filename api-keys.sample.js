965b33ea9dbb1b8d5cdb38a440d7a355510f3124
Michael Houmann <mhoumann87@gmail.com>
2021-06-10 08:51:56 +0200

    Initial commit :tada:

---
+++ api-keys.sample.js
@@ -0,0 +1,5 @@
+/*
+  Insert your own api key below and delete the .sample in the file name
+*/
+
+module.exports = 'YOUR API_KEY GOES HERE';
---
+++ app.js
@@ -0,0 +1,3 @@
+const apiKey = require('./api-keys');
+
+console.log(apiKey);
