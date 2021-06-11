d567b7972c06d74e86b8776ba21512b2c1dacf9f
Michael Houmann <mhoumann87@gmail.com>
2021-06-11 10:13:10 +0200

    Add simple error handeling for api calls

--- app.js
+++ app.js
@@ -13,15 +13,26 @@ const geoUrl = `http://api.positionstack.com/v1/forward?access_key=${apiKey.geo}
 const geoEncodedUrl = encodeURI(geoUrl);
 
 request({ url: geoEncodedUrl, json: true }, (err, res) => {
-  lat = res.body.data[0].latitude;
-  long = res.body.data[0].longitude;
-
-  console.log(lat, long);
+  if (error) {
+    console.error('Unable to connect to the network');
+  } else if (res.body.error) {
+    console.error('Unable to find location');
+  } else {
+    lat = res.body.data[0].latitude;
+    long = res.body.data[0].longitude;
+    console.log(lat, long);
+  }
 });
 
 // request({ url: weatherEncodedUrl, json: true }, (err, res) => {
 //   const info = res.body.current;
+//   if (error) {
+//     console.error('Unable to connect to the weather network');
+//   } else if () {
+//     console.error('Unable to find location');
+//   } else {
 //   console.log(
 //     `The temperature right now is ${info.temperature} degrees, it feels like ${info.feelslike} and the weather is ${info.weather_descriptions}.`
 //   );
+//   }
 // });
