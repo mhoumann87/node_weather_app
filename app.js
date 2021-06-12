99b7bb090f7e86807fe425d3bca1fdd322ecf1f4
Michael Houmann <mhoumann87@gmail.com>
2021-06-12 16:19:59 +0200

    Refactor geoCode call in app.js

--- app.js
+++ app.js
@@ -8,22 +8,19 @@ if (!location) {
   return console.error('Please provide a location');
 }
 
-geoCode(location, (error, geoData) => {
+geoCode(location, (error, { longitude, latitude, location, country } = {}) => {
   if (error) {
     return console.error(error);
   }
 
-  if (error) {
-    return console.error(error);
-  }
-  weather(geoData.latitude, geoData.longitude, (error, weatherData) => {
+  weather(latitude, longitude, (error, weatherData) => {
     if (error) {
       return console.error(error);
     }
     const msg = `
-        The weather in ${geoData.location}, ${geoData.country} is:
-        ${weatherData.weather_descriptions}, the temperature is ${weatherData.temperature} and it feels like ${weatherData.feelslike}.
-      `;
+          The weather in ${location}, ${country} is:
+          ${weatherData.weather_descriptions}, the temperature is ${weatherData.temperature} and it feels like ${weatherData.feelslike}.
+        `;
     console.log(msg);
   });
 });
