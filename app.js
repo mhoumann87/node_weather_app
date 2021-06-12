94a6e13c676df1c87c7fb61016ebe7bbd472bbc9
Michael Houmann <mhoumann87@gmail.com>
2021-06-12 10:52:38 +0200

    Connect the two api calls, and get a single output

--- app.js
+++ app.js
@@ -2,11 +2,19 @@
 const geoCode = require('./utils/geocode');
 const weather = require('./utils/weather');
 
-geoCode('Oslo', (error, data) => {
-  console.log(error, data);
-});
+geoCode('Oslo', (error, geoData) => {
+  if (error) {
+    return console.error(error);
+  }
 
-weather('59.974453', '10.735045', (error, data) => {
-  console.log(error);
-  console.log(data);
+  weather(geoData.latitude, geoData.longitude, (error, weatherData) => {
+    if (error) {
+      return console.error(error);
+    }
+    const msg = `
+      The weather in ${geoData.location}, ${geoData.country} is:
+      ${weatherData.weather_descriptions}, the temperature is ${weatherData.temperature} and it feels like ${weatherData.feelslike}.  
+    `;
+    console.log(msg);
+  });
 });
